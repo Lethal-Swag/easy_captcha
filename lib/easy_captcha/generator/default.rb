9197eb5402b3695ad3596f030b3ddc785d86be0a
Manpreet Singh <msingh@quark.com>
2023-09-13 17:08:40 +0530

    add missing attr accessor

--- lib/easy_captcha/generator/default.rb
+++ lib/easy_captcha/generator/default.rb
@@ -28,7 +28,7 @@ module EasyCaptcha
       end
 
       # Font
-      attr_accessor :font_size, :font_fill_color, :font, :font_family, :font_stroke, :font_stroke_color, :gravity
+      attr_accessor :font_size, :font_fill_color, :font, :font_family, :font_stroke, :font_stroke_color, :gravity, :font_weight, :fill, :stroke, :stroke_width, :pointsize
 
       # Background
       attr_accessor :image_background_color, :background_image, :background_color
