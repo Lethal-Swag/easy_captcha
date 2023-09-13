3b3a933ef197e01016ba7296ddeba6302fc9841a
Manpreet Singh <msingh@quark.com>
2023-09-13 19:06:25 +0530

    change image code

--- lib/easy_captcha/generator/default.rb
+++ lib/easy_captcha/generator/default.rb
@@ -58,63 +58,91 @@ module EasyCaptcha
       end
 
       # generate image
+      # def generate(code)
+      #   require 'rmagick' unless defined?(Magick)
+
+      #   config = self
+      #   canvas = Magick::Image.new(EasyCaptcha.image_width, EasyCaptcha.image_height) do |variable|
+      #     self.background_color = config.image_background_color unless config.image_background_color.nil?
+      #     self.background_color = 'none' if config.background_image.present?
+      #   end
+
+      #   # Render the text in the image
+      #   canvas.annotate(Magick::Draw.new, 0, 0, 0, 0, code) {
+      #     self.gravity     = Magick::CenterGravity
+      #     self.font        = config.font
+      #     self.font_weight = Magick::LighterWeight
+      #     self.fill        = config.font_fill_color
+      #     if config.font_stroke.to_i > 0
+      #       self.stroke       = config.font_stroke_color
+      #       self.stroke_width = config.font_stroke
+      #     end
+      #     self.pointsize = config.font_size
+      #   }
+
+      #   # Blur
+      #   canvas = canvas.blur_image(config.blur_radius, config.blur_sigma) if config.blur?
+
+      #   # Wave
+      #   w = config.wave_length
+      #   a = config.wave_amplitude
+      #   canvas = canvas.wave(rand(a.last - a.first) + a.first, rand(w.last - w.first) + w.first) if config.wave?
+
+      #   # Sketch
+      #   canvas = canvas.sketch(config.sketch_radius, config.sketch_sigma, rand(180)) if config.sketch?
+
+      #   # Implode
+      #   canvas = canvas.implode(config.implode.to_f) if config.implode.is_a? Float
+
+      #   # Crop image because to big after waveing
+      #   canvas = canvas.crop(Magick::CenterGravity, EasyCaptcha.image_width, EasyCaptcha.image_height)
+
+
+      #   # Combine images if background image is present
+      #   if config.background_image.present?
+      #     background = Magick::Image.read(config.background_image).first
+      #     background.composite!(canvas, Magick::CenterGravity, Magick::OverCompositeOp)
+
+      #     image = background.to_blob { self.format = MagickFormat.Png }
+      #   else
+      #     image = canvas.to_blob { self.format = MagickFormat.Png }
+      #   end
+
+      #   # ruby-1.9
+      #   image = image.force_encoding 'UTF-8' if image.respond_to? :force_encoding
+
+      #   canvas.destroy!
+      #   image
+      # end
+
       def generate(code)
-        require 'rmagick' unless defined?(Magick)
+          image = create_image(EasyCaptcha.image_width, EasyCaptcha.image_height)
+          draw_text(code, image)
+          data = image.to_blob
+          image.destroy!
+          data
+      end
 
-        config = self
-        canvas = Magick::Image.new(EasyCaptcha.image_width, EasyCaptcha.image_height) do |variable|
-          Rails.logger.info("captcha config -> #{config.image_background_color}, #{config.background_color}")
-          self.background_color = config.image_background_color unless config.image_background_color.nil?
-          self.background_color = 'none' if config.background_image.present?
-        end
-
-        # Render the text in the image
-        canvas.annotate(Magick::Draw.new, 0, 0, 0, 0, code) {
-          self.gravity     = Magick::CenterGravity
-          self.font        = config.font
-          self.font_weight = Magick::LighterWeight
-          self.fill        = config.font_fill_color
-          if config.font_stroke.to_i > 0
-            self.stroke       = config.font_stroke_color
-            self.stroke_width = config.font_stroke
-          end
-          self.pointsize = config.font_size
-        }
-
-        # Blur
-        canvas = canvas.blur_image(config.blur_radius, config.blur_sigma) if config.blur?
-
-        # Wave
-        w = config.wave_length
-        a = config.wave_amplitude
-        canvas = canvas.wave(rand(a.last - a.first) + a.first, rand(w.last - w.first) + w.first) if config.wave?
-
-        # Sketch
-        canvas = canvas.sketch(config.sketch_radius, config.sketch_sigma, rand(180)) if config.sketch?
-
-        # Implode
-        canvas = canvas.implode(config.implode.to_f) if config.implode.is_a? Float
-
-        # Crop image because to big after waveing
-        canvas = canvas.crop(Magick::CenterGravity, EasyCaptcha.image_width, EasyCaptcha.image_height)
-
-
-        # Combine images if background image is present
-        if config.background_image.present?
-          background = Magick::Image.read(config.background_image).first
-          background.composite!(canvas, Magick::CenterGravity, Magick::OverCompositeOp)
-
-          image = background.to_blob { self.format = MagickFormat.Png }
-        else
-          image = canvas.to_blob { self.format = MagickFormat.Png }
-        end
-
-        # ruby-1.9
-        image = image.force_encoding 'UTF-8' if image.respond_to? :force_encoding
-
-        canvas.destroy!
-        Rails.logger.info("Image -> #{image}")
+      def create_image(width, height)
+        image = Magick::Image.new(width, height)
+        image.format = "jpg"
+        image.gravity = Magick::CenterGravity
+        image.background_color = 'white'
+  
         image
+      end
+
+      def draw_text(code, image)
+        draw = Magick::Draw.new
+  
+        draw.annotate(image, image.columns, image.rows, 0, 0, code) {
+          self.gravity = Magick::CenterGravity
+          self.pointsize = 22
+          self.fill = 'darkblue'
+          self.stroke = 'transparent'
+        }
+  
+        nil
       end
 
     end
